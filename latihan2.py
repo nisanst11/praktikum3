712205bcdeea1f7195bd89ea0e4913b57f0757d6
khairunnisanst <khairunnisanst785@gmail.com>
2022-10-30 20:46:13 +0700

    menambahkan file README.md

---
+++ latihan2.py
@@ -0,0 +1,12 @@
+a=input("Masukan nilai a : ")
+b=input("Masukan nilai b : ")
+print("Variabel a =",a)
+print("Variabel b =",b)
+print("Hasil penggabungan {1}&{0}=%s".format(a,b) %(a+b))
+
+# Konversi nilai variabel
+a=int(a)
+b=int(b)
+print("penjumlahan {1}&{0}=%s".format(a,b) %(a+b))
+print("pembagian {1}/{0}=%s".format(a,b) %(a/b))
+print("~~~~~~~~~~~~~~~~~~~~~~")8
---
+++ latihan3.py
@@ -0,0 +1,7 @@
+print('{0:>20}'.format(' * '))
+print('{0:>21}'.format(' *** '))
+print('{0:>22}'.format(' ***** '))
+print('{0:>23}'.format(' ******* '))
+print('{0:>22}'.format(' ***** '))
+print('{0:>21}'.format(' *** '))
+print('{0:>20}'.format(' * ')
